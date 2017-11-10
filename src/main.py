081d34932965622e0d3d35a54fbaa8b4b6685bfe
Nicolas Wipfler <nicolas.wipfler@gmx.de>
2017-11-10 12:15:30 +0100

    Added data inspection methods.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,24 @@
+import pandas as pd
+
+def inspect_dataset(dataset):
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_colwidth', 50)
+
+    # Access column id
+    #print(dataset["id"])
+    # Join Column names and print them 
+    #print('\n'.join(list(dataset)))
+    # Example values
+    print(dataset.iloc[7])
+    
 def main():
-    print("Executed main.")
+    print('Listings Subset: ' + '\n')
+    listings_subset = pd.read_csv('../data/subset/listings_sub.csv')
+    inspect_dataset(listings_subset)
+
+    print(('\n' * 2) + 'Reviews Subset: ' + '\n')
+    reviews_subset = pd.read_csv('../data/subset/reviews_sub.csv')
+    inspect_dataset(reviews_subset)
 
 if __name__ == '__main__':
     main()
