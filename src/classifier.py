99b4aadca9a04412752d75ef6f6e5c5dfab02bff
KWeiher <kerstin.weiher92@web.de>
2017-11-28 23:09:58 +0100

    Para Tuning KNN

--- src/classifier.py
+++ src/classifier.py
@@ -318,3 +318,26 @@ class Classifier(object):
         grid_search_estimator.fit(data, target)
 
         print("best score is {} with params {}".format(grid_search_estimator.best_score_, grid_search_estimator.best_params_ ))
+
+    # Parameter Tuning k-NN
+    def para_tuning_KNN(self):
+        clf = KNeighborsClassifier()
+        #parameter = clf.get_params()
+        #print(parameter)
+        target = self.label
+        data = self.data_encoded
+        self.grid_search_knn(data = data, target=target)
+
+    def grid_search_knn(self, data, target):
+        clf = KNeighborsClassifier()
+        print('using 10 Fold Cross-Validation for Classifier k-NN')
+        parameters = {
+            'n_neighbors':[2,3,4,5,6,7], 
+            'algorithm':['ball_tree', 'kd_tree', 'brute']
+        }
+        cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+
+        grid_search_estimator = GridSearchCV(clf, parameters, scoring='accuracy', cv=cv)
+        grid_search_estimator.fit(data,target)
+
+        print("best score is {} with params {}".format(grid_search_estimator.best_score_, grid_search_estimator.best_params_ ))
